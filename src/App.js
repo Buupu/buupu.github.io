21e2a6e66ea6ceced7587c5fb9681657251c3a2e
Buupu <sammyfattah@gmail.com>
2020-08-17 15:06:22 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
+
+import Home from "./pages/home/home";
+import About from "./pages/about/about";
+import Experience from "./pages/experience/experience";
+import Portfolio from "./pages/portfolio/portfolio";
+import Contact from "./pages/contact/contact";
+
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/experience">
+            <Experience />
+          </Route>
+          <Route path="/portfolio">
+            <Portfolio />
+          </Route>
+          <Route path="/contact">
+            <Contact />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

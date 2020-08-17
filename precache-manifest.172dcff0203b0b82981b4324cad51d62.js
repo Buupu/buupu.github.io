181ead0a856f7b9be6f071b7417086412ff901b7
Buupu <sammyfattah@gmail.com>
2020-08-17 15:09:06 +0100

    Updates

---
+++ precache-manifest.172dcff0203b0b82981b4324cad51d62.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bea3585b707616764ea175efec7abba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "693797d8b98c4360dfb0",
+    "url": "/static/css/main.bffd8b14.chunk.css"
+  },
+  {
+    "revision": "1fbbe73ca809d1576b79",
+    "url": "/static/js/2.a32028f6.chunk.js"
+  },
+  {
+    "revision": "0fb1ab9166e4090d40b886200af7baec",
+    "url": "/static/js/2.a32028f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "693797d8b98c4360dfb0",
+    "url": "/static/js/main.e788c7fa.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "18b7e737c82248fb5266971d9ea6cae3",
+    "url": "/static/media/bottomRightFrame.18b7e737.svg"
+  },
+  {
+    "revision": "d049fabcd2218d4c566c4ba3521ed9c7",
+    "url": "/static/media/github.d049fabc.svg"
+  },
+  {
+    "revision": "d145c152a4f2de64408b6d2acd562c09",
+    "url": "/static/media/instagram.d145c152.svg"
+  },
+  {
+    "revision": "fcee6acebb15daf87c33fcea7a679186",
+    "url": "/static/media/linkedin.fcee6ace.svg"
+  },
+  {
+    "revision": "9f1fb18b67c4305c0fccd8c6a52e937f",
+    "url": "/static/media/logo.9f1fb18b.svg"
+  },
+  {
+    "revision": "370848707ebdd5ad99cb812c6cdfec3a",
+    "url": "/static/media/pen.37084870.svg"
+  },
+  {
+    "revision": "28ff9930f356259877d5e4322c11d165",
+    "url": "/static/media/sammyfattah.28ff9930.JPG"
+  },
+  {
+    "revision": "52269b22432554d924b64d1118d96997",
+    "url": "/static/media/topLeftFrame.52269b22.svg"
+  }
+]);

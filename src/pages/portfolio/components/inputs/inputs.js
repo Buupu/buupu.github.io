28b8f6757decc2fbfb60cd85a7048365f4c92cd0
Buupu <sammyfattah@gmail.com>
2020-08-25 15:52:18 +0100

    Added input component

--- src/pages/portfolio/components/inputs/inputs.js
+++ src/pages/portfolio/components/inputs/inputs.js
@@ -1,6 +1,34 @@
 import React, { useState } from "react";
 import FrameContainer from "../../../../components/frameContainer/frameContainer";
+import Input1 from "./input1/input1";
+const INPUT_STATES = ["CLEAR", "SUCCESS", "ERROR"];
 export default function Inputs(props) {
   const [componentIndex, setComponentIndex] = useState(0);
-  return <FrameContainer></FrameContainer>;
+  const [inputState, setInputState] = useState(INPUT_STATES[0]);
+  return (
+    <FrameContainer>
+      <Input1 state={inputState} />
+      <button
+        onClick={() => {
+          setInputState(INPUT_STATES[1]);
+        }}
+      >
+        show success
+      </button>
+      <button
+        onClick={() => {
+          setInputState(INPUT_STATES[2]);
+        }}
+      >
+        show error
+      </button>
+      <button
+        onClick={() => {
+          setInputState(INPUT_STATES[0]);
+        }}
+      >
+        clear
+      </button>
+    </FrameContainer>
+  );
 }

48d27229d4861343b43cddb46ead1564a564ea1a
Buupu <sammyfattah@gmail.com>
2020-09-18 20:30:37 +0100

    Changed project font

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -3,6 +3,8 @@ import FrameContainer from "../../components/frameContainer/frameContainer";
 import MyImage from "../../assets/sammyfattah.JPG";
 import "./about.css";
 import gsap from "gsap";
+import { ReactComponent as BulletPoint } from "../../assets/bulletPoint.svg";
+import { NavLink } from "react-router-dom";
 export default function About(props) {
   let textRef = useRef();
 
@@ -30,23 +32,42 @@ export default function About(props) {
           user experiences while being performant and robust.
         </p>
         <p>
-          I attended Glasgow's University of Strathclyde where I studied
-          Computer Information Science and graduated with a Bachelors of Science
-          degree with First Class honours.
+          I attended Glasgow's{" "}
+          <a href="https://www.strath.ac.uk/">University of Strathclyde</a>{" "}
+          where I studied Computer Information Science and graduated with a
+          Bachelors of Science degree with First Class honours.
         </p>
         <p>
           Since graduating I have worked independently and within development
           teams on a variety of interesting and meaningful projects in
           production.
         </p>
-        <p>
-          Here are a few technologies I’ve been working with recently: React
-          HTML & (S)CSS Javascript (ES6+) GSAP Figma Node.js
-        </p>
+        <p>Here are a few technologies I’ve been working with recently:</p>
+        <ul>
+          {[
+            <span>React</span>,
+            <span>HTML & (S)CSS</span>,
+            <span>Javascript (ES6+)</span>,
+            <span>GSAP</span>,
+            <span>Figma</span>,
+            <span>Node.js</span>,
+          ].map((technology) => {
+            return (
+              <li>
+                <BulletPoint />
+                {technology}
+              </li>
+            );
+          })}
+        </ul>
         <p>
           I am currently working on freelance projects but I’m always looking
-          for an exciting opportunity. Reach out to me through the contact page,
-          or send me an email at sammyfattah@gmail.com!
+          for an exciting opportunity. Reach out to me through the{" "}
+          <NavLink activeClassName="active" to="/contact">
+            contact
+          </NavLink>{" "}
+          page, or send me an email at{" "}
+          <a href="mailto: sammyfattah@gmail.com">sammyfattah@gmail.com</a>!
         </p>
       </div>
       <FrameContainer animated>

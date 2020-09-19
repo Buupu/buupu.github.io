ef6d74c7a11fe132cdec13e8615057d2cf2ddb58
Buupu <sammyfattah@gmail.com>
2020-09-19 13:11:09 +0100

    Added call to action on home page

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -2,6 +2,7 @@ import React from "react";
 import FrameContainer from "../../components/frameContainer/frameContainer";
 import "./home.css";
 import { ReactComponent as Logo } from "../../assets/logo.svg";
+import { Link } from "react-router-dom";
 export default function Home(props) {
   return (
     <div className="Home">
@@ -9,7 +10,10 @@ export default function Home(props) {
         <div className="Home__LogoContainer">
           <Logo />
           <h1>Sammy Fattah</h1>
-          <h2>Web Developer</h2>
+          <h2>Web Development</h2>
+          <Link to="/portfolio">
+            <button className="Home__CTA">View my work</button>
+          </Link>
         </div>
       </FrameContainer>
     </div>

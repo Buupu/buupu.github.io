6468274fba247aba7f300aeec319e6bbd06d35d5
Buupu <sammyfattah@gmail.com>
2021-01-04 09:31:23 +0000

    Removed unused function

--- src/App.js
+++ src/App.js
@@ -7,19 +7,6 @@ import Portfolio from "./assets/js/pages/portfolio";
 import About from "./assets/js/pages/about";
 
 function App() {
-  function hashLinkScroll() {
-    const { hash } = window.location;
-    if (hash !== "") {
-      // Push onto callback queue so it runs after the DOM is updated,
-      // this is required when navigating from a different page so that
-      // the element is rendered on the page before trying to getElementById.
-      setTimeout(() => {
-        const id = hash.replace("#", "");
-        const element = document.getElementById(id);
-        if (element) element.scrollIntoView();
-      }, 0);
-    }
-  }
   return (
     <Router>
       <div className="wrapper">

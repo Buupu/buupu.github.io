6620b5d89f9326884ebb0b8e537d9c8e5c1ae6f0
Buupu <sammyfattah@gmail.com>
2021-01-04 21:32:31 +0000

    Header progress

--- src/App.js
+++ src/App.js
@@ -1,37 +1,23 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+/*--------- Page Imports ----------*/
 import Home from "./assets/js/pages/home";
 import Contact from "./assets/js/pages/contact";
 import Experience from "./assets/js/pages/experience";
 import Portfolio from "./assets/js/pages/portfolio";
 import About from "./assets/js/pages/about";
 
+/*--------- Component Imports ----------*/
+import Header from "./assets/js/components/header";
+
 function App() {
   return (
     <Router>
       <div className="wrapper">
-        <nav className="main-menu-container">
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/portfolio">Portfolio</Link>
-            </li>
-            <li>
-              <Link to="/experience">Experience</Link>
-            </li>
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-          </ul>
-        </nav>
+        <Header />
+
         <div className="twenty-spacer" />
-        {/* A <Switch> looks through its children <Route>s and
-        renders the first one that matches the current URL. */}
         <Switch>
           <Route path="/contact">
             <Contact />
---
+++ src/assets/js/components/header.js
@@ -0,0 +1,102 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+import Logo from "../../img/logo.png";
+export default function Header(props) {
+  const resumeHref =
+    "https://github.com/Buupu/CV/blob/743c2621b52c8abc2ebfd2562104e5fe75d8a979/Sammy%20Fattah%20-%20CV.pdf";
+
+  return (
+    <header class="content">
+      <div class="main-header hide-on-responsive">
+        <div class="main-header-container">
+          <div class="one-flex-margin-container">
+            <div class="logo-container">
+              <div class="logo-holder">
+                <a>
+                  <img src={Logo} width="34px" height="34px" alt="S Logo" />
+                </a>
+              </div>
+            </div>
+
+            <div class="main-header-cta-middle-holder">
+              <nav className="main-menu-container">
+                <ul>
+                  <li>
+                    <NavLink activeClassName="is-active" exact to="/">
+                      Home
+                      <span className="active-marker"></span>
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink activeClassName="is-active" exact to="/about">
+                      About
+                      <span className="active-marker"></span>
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink activeClassName="is-active" exact to="/portfolio">
+                      Portfolio
+                      <span className="active-marker"></span>
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink activeClassName="is-active" exact to="/experience">
+                      Experience
+                      <span className="active-marker"></span>
+                    </NavLink>
+                  </li>
+                  <li>
+                    <NavLink activeClassName="is-active" exact to="/contact">
+                      Contact
+                      <span className="active-marker"></span>
+                    </NavLink>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+            <div className="main-header-cta-right-holder">
+              <nav className="main-menu-container">
+                <ul>
+                  <li>
+                    <a href={resumeHref} target="_blank">
+                      View Resume
+                    </a>
+                  </li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="mobile-header show-on-responsive">
+        <div class="main-header-container">
+          <div class="one-flex-margin-container">
+            <div class="logo-container">
+              <div class="logo-holder">
+                <a href="">
+                  <img src={Logo} width="34px" height="34px" alt="S Logo" />
+                </a>
+              </div>
+            </div>
+            <div class="mobile-burger-outer-container">
+              <div class="burger-holder" id="hamburger-6">
+                <span class="line"></span>
+                <span class="line"></span>
+                <span class="line"></span>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="mobile-menu-container">
+          <div class="mobile-main-menu-holder"></div>
+          <div class="mobile-menu-footer-container navy-background">
+            <div class="mobile-search-holder">
+              <form role="search" action="" method="get"></form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+}
